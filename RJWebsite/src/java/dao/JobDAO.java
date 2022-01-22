c5b52b02bec127c6eee8714167a2c3ac9e18e1a9
dat-gif <70569870+dat-gif@users.noreply.github.com>
2022-01-22 17:14:48 +0700

    [Code - JobDAO][getSkillByJobId()]

--- RJWebsite/src/java/dao/JobDAO.java
+++ RJWebsite/src/java/dao/JobDAO.java
@@ -8,6 +8,9 @@ package dao;
 import IDao.IJob;
 import context.DBContext;
 import entity.Job;
+import entity.Recruiter;
+import entity.Skill;
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -22,16 +25,17 @@ public class JobDAO implements IJob {
 
     Connection conn = null;//ket noi sql
     PreparedStatement ps = null; //truyen querry sang sql
-    ResultSet rs = null; //nhan tra ve
+    ResultSet rs = null; //nhan tra ve, với các func gọi chéo nhau, nên tạo rs riêng
 
     /**
      * Get first 12 job record in database
      *
-     * @return list of Job 
+     * @return list of Job
      */
     @Override
     public ArrayList<Job> getJobLandingPage() {
         ArrayList<Job> list = new ArrayList<>();
+
         String query = "SELECT TOP (12) [job_id]\n"
                 + "      ,[recruiter_id]\n"
                 + "      ,[title]\n"
@@ -47,36 +51,68 @@ public class JobDAO implements IJob {
         try {
             conn = new DBContext().getConnection();
             ps = conn.prepareStatement(query);
-            rs = ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                list.add(new Job(rs.getInt("job_id"),
+                Job job = new Job(rs.getInt("job_id"),
                         rs.getInt("recruiter_id"),
-                        rs.getString("title"),
-                        rs.getString("description"),
-                        rs.getString("salary_range"),
-                        rs.getString("quantity"),
-                        rs.getString("role"),
-                        rs.getString("experience"),
-                        rs.getString("location"),
-                        rs.getString("hire_date"),
+                        rs.getString("title").trim(),
+                        rs.getString("description").trim(),
+                        rs.getString("salary_range").trim(),
+                        rs.getString("quantity").trim(),
+                        rs.getString("role").trim(),
+                        rs.getString("experience").trim(),
+                        rs.getString("location").trim(),
+                        rs.getString("hire_date").trim(),
                         rs.getBoolean("status")
-                ));
+                );
+                job.setSkillList(getSkillByJobId(job.getjId()));
+                list.add(job);
             }
         } catch (Exception e) {
             System.out.println("Bug :" + e);
         }
-
         return list;
     }
-// Test JobDao
 
+    @Override
+    public ArrayList<Skill> getSkillByJobId(int jobId) {
+        ArrayList<Skill> skillList = new ArrayList<>();
+        String query = "SELECT skill.skill_id, skill.name, skill.icon, skill.description\n"
+                + "  FROM [SWP391].[dbo].[job]\n"
+                + "  inner join job_skill on job.job_id = job_skill.job_id\n"
+                + "  inner join skill on job_skill.skill_id= skill.skill_id\n"
+                + "  where job.job_id= ? ";
+
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, jobId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                skillList.add(new Skill(rs.getInt("skill_id"),
+                        rs.getNString("name").trim(),
+                        rs.getNString("icon").trim(),
+                        rs.getString("description").trim()));
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return skillList;
+    }
+
+    public Recruiter getRecruterByJobId(int jobId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+// Test JobDao
     public static void main(String[] args) {
         IJob jobDao = new JobDAO();
         ArrayList<Job> testList = jobDao.getJobLandingPage();
         for (Job job : testList) {
-            job.toString();
-            System.out.println("1" + job.toString());
-        }
-    }
 
+            System.out.println(job.toString());
+        }
+        System.out.println(testList.size());
+    }
 }
