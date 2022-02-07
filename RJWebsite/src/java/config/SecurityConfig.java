0fe77a4cd8fd5d2db9c1b68898f217708361afc8
dat-gif <70569870+dat-gif@users.noreply.github.com>
2022-02-07 15:00:13 +0700

    [Code-SecurityConfig][Create demo SecurityConfig class ]

---
+++ RJWebsite/src/java/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author Admin
+ */
+public class SecurityConfig {
+
+    public static final String ROLE_CANDIDATE = "CANDIDATE";
+    public static final String ROLE_RECRUITER = "RECRUITER";
+    public static final String ROLE_ADMIN = "ADMIN";
+
+// String: Role
+// List<String>: urlPatterns.
+    private static final Map<String, List<String>> mapConfig = new HashMap<String, List<String>>();
+
+    static {
+        init();
+    }
+
+    private static void init() {
+
+        // Cấu hình cho vai trò "CANDIDATE".
+        List<String> urlPatterns1 = new ArrayList<String>();
+
+        urlPatterns1.add("/searchjob");
+        urlPatterns1.add("/searchcompany");
+
+        mapConfig.put(ROLE_CANDIDATE, urlPatterns1);
+
+        // Cấu hình cho vai trò "RECRUITER".
+        List<String> urlPatterns2 = new ArrayList<String>();
+
+        urlPatterns2.add("/RecruiterDashBoard");
+
+        mapConfig.put(ROLE_RECRUITER, urlPatterns2);
+    }
+
+    public static Set<String> getAllAppRoles() {
+        return mapConfig.keySet();
+    }
+
+    public static List<String> getUrlPatternsForRole(String role) {
+        return mapConfig.get(role);
+    }
+}
