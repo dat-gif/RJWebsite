4a15534dc703f3220af29003a5f83783724e262b
dat-gif <70569870+dat-gif@users.noreply.github.com>
2022-02-21 14:48:40 +0700

    [Update-JobDAO][Thêm search bảng skill khi search job ]

--- RJWebsite/src/java/dao/JobDAO.java
+++ RJWebsite/src/java/dao/JobDAO.java
@@ -459,21 +459,25 @@ public class JobDAO extends DBContext implements IJob {
                 + "      ,[createAt]\n"
                 + "      ,[updateAt])\n";
 
-        String freeTextQuery = " select job.[job_id]\n"
-                + "      ,job.[recruiter_id]\n"
-                + "      ,job.[title]\n"
-                + "      ,job.[description]\n"
-                + "      ,job.[salary_range]\n"
-                + "      ,job.[quantity]\n"
-                + "      ,job.[role]\n"
-                + "      ,job.[experience]\n"
-                + "      ,job.[location]\n"
-                + "      ,job.[hire_date]\n"
-                + "      ,job.[questions]\n"
-                + "      ,job.[status]\n"
-                + "      ,job.[createAt]\n"
-                + "      ,job.[updateAt]\n"
-                + " from job where freetext(title, ? )\n";
+        String freeTextQuery = " select DISTINCT job.[job_id]\n"
+                + ",job.[recruiter_id]\n"
+                + ",job.[title]\n"
+                + ",job.[description]\n"
+                + ",job.[salary_range]\n"
+                + ",job.[quantity]\n"
+                + ",job.[role]\n"
+                + ",job.[experience]\n"
+                + ",job.[location]\n"
+                + ",job.[hire_date]\n"
+                + ",job.[questions]\n"
+                + ",job.[status]\n"
+                + ",job.[createAt]\n"
+                + ",job.[updateAt]\n"
+                + "from job\n"
+                + "left join job_skill on job_skill.job_id = job.job_id\n"
+                + "left join skill on job_skill.skill_id = skill.skill_id\n"
+                + "where (freetext(job.title, ? ) or freetext(skill.name ,?)) and job.status=1\n";
+
         String mainQuery = clearQuery + insertTable + freeTextQuery;
 
         if (!skillValue.equalsIgnoreCase("All") || !cityValue.equalsIgnoreCase("All")) {
@@ -497,10 +501,11 @@ public class JobDAO extends DBContext implements IJob {
             mainQuery = mainQuery + joinQuery + queryWhere;
         }
         try {
-            int count = 1;
+            int count = 2;
             Connection conn = new DBContext().getConnection();
             PreparedStatement ps = conn.prepareStatement(mainQuery);
-            ps.setString(count, txtSearch);
+            ps.setString(1, txtSearch);
+            ps.setString(2, txtSearch);
             if (!skillValue.equalsIgnoreCase("All") || !cityValue.equalsIgnoreCase("All")) {
                 count++;
                 if (!skillValue.equalsIgnoreCase("All")) {
