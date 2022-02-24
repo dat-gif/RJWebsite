e9d1cd6994eb410f41c2f0c811ea534ff8593513
dat-gif <70569870+dat-gif@users.noreply.github.com>
2022-02-24 21:40:47 +0700

    [CandidateDAO][getCandidateProfileById(int account_id)]

--- RJWebsite/src/java/dao/CandidateDAO.java
+++ RJWebsite/src/java/dao/CandidateDAO.java
@@ -11,18 +11,75 @@ import entity.Candidate;
 import entity.CandidateCV;
 import entity.CandidatePrize;
 import entity.Certificate;
+import entity.City;
 import entity.Education;
 import entity.Social;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
+ * The data access object executes a data query from the Candidate table or main
+ * relate with Candidate information.
  *
  * @author Admin
  */
-public class CandidateDAO extends DBContext implements ICandidate{
+public class CandidateDAO extends DBContext implements ICandidate {
 
+    /**
+     * Get basic info about candidate like email, address,etc...
+     *
+     * @param account_id int, id account access to website
+     * @return
+     */
     @Override
-    public Candidate getCandidateProfileById(int candidateId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Candidate getCandidateProfileById(int account_id) {
+        String query = "SELECT [candidate_id]\n"
+                + "      ,account.account_id\n"
+                + "      ,[first_name]\n"
+                + "      ,[last_name]\n"
+                + "      ,[birth_date]\n"
+                + "      ,[address]\n"
+                + "      ,[avatar]\n"
+                + "      ,[sex]\n"
+                + "      ,[banner]\n"
+                + "      ,account.phone\n"
+                + "      ,[finding_job]\n"
+                + "      ,[cv_manage_id]\n"
+                + "      ,[experience_manage_id]\n"
+                + "      ,[education_mange_id]\n"
+                + "      ,[social_manage_id]\n"
+                + "      ,[project_manage_id]\n"
+                + "      ,[certificate_manage_id]\n"
+                + "      ,[prize_manage_id]\n"
+                + "      ,[city]\n"
+                + "	  ,email\n"
+                + "  FROM [SWP391].[dbo].[candidate]\n"
+                + "  inner join account on account.account_id= candidate.account_id\n"
+                + "  Where account.account_id=?";
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Candidate candidate = new Candidate();
+                candidate.setCandIdateId(rs.getInt("candidate_id"));
+                candidate.setAccountId(rs.getInt("account_id"));
+                candidate.setFirstName(rs.getString("first_name"));
+                candidate.setLastName(rs.getString("last_name"));
+                candidate.setAvatar(rs.getString("avatar"));
+                candidate.setBirthDate(rs.getString("birth_date"));
+                candidate.setGender(rs.getBoolean("sex"));
+                candidate.setBanner(rs.getString("banner"));
+                candidate.setPhone(rs.getString("account.phone"));
+                candidate.setFindingJob(rs.getBoolean("finding_job"));
+                candidate.setEmail(rs.getString("email"));
+                return candidate;
+            }
+
+        } catch (Exception e) {
+        }
+        return null;
     }
 
     @Override
@@ -54,5 +111,5 @@ public class CandidateDAO extends DBContext implements ICandidate{
     public Social getSocialByCandidateId(int candidateId) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
